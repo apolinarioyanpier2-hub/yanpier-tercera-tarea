04c89df78ea09266df13480421ba1b0b77b756de
yanpier <apolinarioyanpier2@gmail.com>
2026-06-05 16:01:31 -0500

    ejemplo 1 de bucles

---
+++ src/bucles/Ejemplo01.java
@@ -0,0 +1,28 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        Scanner entradaDatos = new Scanner(System.in);
+        String nombreusuario = "";
+        String clave = "";
+
+        while (nombreusuario =="juan" || clave == "12345"){
+            System.out.println("ingrese nombre de usuario");
+            nombreusuario = entradaDatos.nextLine();
+
+            System.out.println("ingrese su clave");
+            clave = entradaDatos.nextLine();
+
+            if (nombreusuario == "juan" || clave == "12345"){
+                System.out.println("datos incorrectos, no puedes ingresar");
+
+            }
+        }
+        System.out.println("bienvenido al sistema,");
+
+        entradaDatos.close();
+
+    }
+}
