1633367e3bfeae0f4e080696e959c28a65c18c1b
yanpier <apolinarioyanpier2@gmail.com>
2026-06-11 16:56:48 -0500

    ejercicios (para)

---
+++ src/bucles/Para.java
@@ -0,0 +1,18 @@
+package bucles;
+
+public class Para {
+    public static void main(String[] args) {
+        /*
+        SINTAXIS DE CICLO FOR = PARA
+        for (2 inicio; 1 condicion; 3 incremento o actualizacion) {
+        4 bloque de instrucciones a realizar o acciones
+
+         */
+        // crear un algoritmo que permita impremir los numeros del uno al 10,
+
+        for (int valorinicial = 1;valorinicial <=10; valorinicial +=2){
+            System.out.println("la tabla de multiplicar:"+ valorinicial);
+        }
+
+    }
+}
